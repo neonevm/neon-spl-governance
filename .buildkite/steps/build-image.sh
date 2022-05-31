bedfd99594cd5f2043ab683f7f9a8ad357fe1257
mich-master <minyas@yandex.ru>
2022-05-31 11:29:36 +0300

    Buildkite Pipeline Init

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo "Neon Governance revision = ${BUILDKITE_COMMIT}"
 
-set ${SOLANA_REVISION:=v1.9.12}
+set ${SOLANA_REVISION:=v1.9.12-testnet-with_trx_cap}
 
 docker pull solanalabs/solana:${SOLANA_REVISION}
 echo "SOLANA_REVISION=$SOLANA_REVISION"
