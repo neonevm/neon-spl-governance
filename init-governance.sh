aff835c4b4885ae587fac28797dd37b6b0b676ff
mich-master <minyas@yandex.ru>
2022-06-02 12:42:53 +0300

    #45 Bring Tests into CI

--- init-governance.sh
+++ init-governance.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
-solana -v --url http://localhost:8899 --keypair artifacts/creator.keypair airdrop 100
-solana -v --url http://localhost:8899 --keypair artifacts/payer.keypair airdrop 100
+solana -v --keypair artifacts/creator.keypair airdrop 100
+solana -v --keypair artifacts/payer.keypair airdrop 100
 
-solana -v --url http://localhost:8899 --keypair artifacts/voters/voter1.keypair airdrop 100
-solana -v --url http://localhost:8899 --keypair artifacts/voters/voter2.keypair airdrop 100
-solana -v --url http://localhost:8899 --keypair artifacts/voters/voter3.keypair airdrop 100
-solana -v --url http://localhost:8899 --keypair artifacts/voters/voter4.keypair airdrop 100
-solana -v --url http://localhost:8899 --keypair artifacts/voters/voter5.keypair airdrop 100
+solana -v --keypair artifacts/voters/voter1.keypair airdrop 100
+solana -v --keypair artifacts/voters/voter2.keypair airdrop 100
+solana -v --keypair artifacts/voters/voter3.keypair airdrop 100
+solana -v --keypair artifacts/voters/voter4.keypair airdrop 100
+solana -v --keypair artifacts/voters/voter5.keypair airdrop 100
 
-spl-token --url http://localhost:8899 create-token --decimals 6 --fee-payer artifacts/payer.keypair artifacts/community-mint.keypair
+spl-token --url create-token --decimals 6 --fee-payer artifacts/payer.keypair artifacts/community-mint.keypair
 
-solana program deploy --url http://localhost:8899 --program-id artifacts/spl-governance.keypair -v deploy/spl_governance.so
+solana program deploy --url --program-id artifacts/spl-governance.keypair -v deploy/spl_governance.so
 
-solana program deploy --url http://localhost:8899 --program-id artifacts/addin-fixed-weights.keypair -v deploy/spl_governance_addin_fixed_weights.so
+solana program deploy --url --program-id artifacts/addin-fixed-weights.keypair -v deploy/spl_governance_addin_fixed_weights.so
 
-solana program deploy --url http://localhost:8899 --program-id artifacts/addin-vesting.keypair -v deploy/spl_governance_addin_vesting.so
+solana program deploy --url --program-id artifacts/addin-vesting.keypair -v deploy/spl_governance_addin_vesting.so
 
-solana program deploy --url http://localhost:8899 --program-id artifacts/maintenance.keypair -v deploy/maintenance.so
+solana program deploy --url --program-id artifacts/maintenance.keypair -v deploy/maintenance.so
--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
+solana set config -u ${SOLANA_URL:-http://localhost:8899}
+
 ./init-governance.sh
